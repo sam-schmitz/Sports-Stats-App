bf46cac98e743f0d2faf5e290c27772cbd901a49
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-01 15:08:39 -0500

    Updated backend/app.js to connect to a MongoDB stored in a Docker container.

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,22 @@
 // app.js
 //By: Sam Schmitz
 //The app of the back end for the sports statistics tracker
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config({path: __dirname + '/.env'});
+
+//app set up
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//MongoDB connection
+console.log('MONGO_URI:', process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('MongoDB connected locally via Docker'))
+    .catch(err => console.error('MongoDB connection error:', err));
