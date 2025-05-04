bce335b81006ef4b423d04b08b5671695e06f7d2
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-03 19:28:45 -0500

    Created backend/models/Player.js. This model stores information about a player within the database.

---
+++ backend/models/Player.js
@@ -0,0 +1,49 @@
+// Player.js
+// By: Sam Schmitz
+// A model for the MongoDB that stores information about a player
+
+const mongoose = require('mongoose');
+
+const PlayerSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true  // ex: "lebron_james" (use underscores)
+    },
+    sport: {
+        type: String,
+        required: true,
+        enum: ['basketball', 'soccer', 'football', 'baseball', 'hockey']
+    },
+    name: {
+        type: String,
+        required: true  // Full name: "Lebron James"
+    },
+    team_id: {
+        type: String,
+        ref: 'Team',
+        required: true
+    },
+    position: {
+        type: String    // ex: "SF"
+    },
+    jersey_number: {
+        type: Number
+    },
+    height: {
+        type: String    // ex: "6'9\""
+    },
+    weight: {
+        type: String    // ex: "250 lbs"
+    },
+    dob: {
+        type: Date
+    },
+    nationality: {
+        type: String
+    },
+    photoUrl: {
+        type: String
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Player', PlayerSchema);
