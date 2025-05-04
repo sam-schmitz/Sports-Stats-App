b3a0a19a5a0faadd66246c6256bcfaa71a64ce53
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-03 19:18:35 -0500

    Created backend/models/Team.js

---
+++ backend/models/Team.js
@@ -0,0 +1,39 @@
+// Team.js
+// By: Sam Schmitz
+// A model for the MongoDB that stores information about a team
+
+const mongoose = require('mongoose');
+
+const TeamSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true  // ex: "lakers", "browns"
+    },
+    sport: {
+        type: String,
+        required: true,
+        enum: ['basketball', 'soccer', 'football', 'baseball', 'hockey']
+    },
+    name: {
+        type: String,
+        required: true  // full name: "Los Angeles Lakers"
+    },
+    abbreviation: {
+        type: String,
+        required: true // ex: "LAL"
+    },
+    conference: {
+        type: String // Optional, ex: "Western"
+    },
+    division: {
+        type: String    // Optional, ex: "Pacific"
+    },
+    location: {
+        type: String    //Optional, ex: "Los Angeles, CA"
+    },
+    logoUrl: {
+        type: String    //Optional: store team logos
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Team', TeamSchema);
