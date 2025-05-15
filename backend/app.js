664adf460b706d818fec6891b88141a6720e5b2f
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-15 12:56:39 -0500

    Added the teams router to app.

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config({ path: __dirname + '/.env' });    //use just .config(); if in backend folder
+const http = require('http');
+const { Server } = require('socket.io');
+const teamsRouter = require('./routes/teams');
 
 //app set up
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/teams', teamsRouter);
 
 //MongoDB connection
 console.log('MONGO_URI:', process.env.MONGO_URI);
@@ -20,3 +24,22 @@ mongoose.connect(process.env.MONGO_URI, {
 })
     .then(() => console.log('MongoDB connected locally via Docker'))
     .catch(err => console.error('MongoDB connection error:', err));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', socket => {
+    console.log('New client connected');
+
+    socket.on('disconect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
