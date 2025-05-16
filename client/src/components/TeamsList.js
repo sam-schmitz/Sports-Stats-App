97aa291eccfcb6c445fd761a4f8f69b919239a7f
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-16 10:22:35 -0500

    Created components/TeamsList.js as its own page & file.

---
+++ client/src/components/TeamsList.js
@@ -0,0 +1,27 @@
+// components/TeamsList.js
+// By: Sam Schmitz
+
+function TeamsList() {
+    const [teams, setTeams] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/teams')
+            .then(res => setTeams(res.data))
+            .catch(err => console.error('Error fetching teams:', err));
+    }, []);
+
+    return (
+        <div>
+            <h2>NBA Teams</h2>
+            <ul>
+                {teams.map(team => (
+                    <li key={team._id}>
+                        {team.name} ({team.abbreviation})
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TeamsList();
