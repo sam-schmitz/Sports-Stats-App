b537787bc626418d470f0f9a7826f6fcc9649b48
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-16 10:38:15 -0500

    Created components/PlayersList.js to display a list of all players in the NBA.

---
+++ client/src/components/PlayersList.js
@@ -0,0 +1,30 @@
+// PlayersList.js
+// By: Sam Schmitz
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function PlayersList() {
+    const [teams, setTeams] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/players')
+            .then(res => setTeams(res.data))
+            .catch(err => console.error('Error fetching players:', err));
+    }, []);
+
+    return (
+        <div>
+            <h2>NBA Players</h2>
+            <ul>
+                {teams.map(player => (
+                    <li key={player._id}>
+                        {player.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default PlayersList();
