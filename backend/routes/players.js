80508e4a4489aaa0a33a1e10e8d414a75beeae5b
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-26 20:58:27 -0500

    Added the ability to search for a player by name.

--- backend/routes/players.js
+++ backend/routes/players.js
@@ -11,8 +11,24 @@ router.get('/', async (req, res) => {
         const players = await Player.find();
         res.json(players);
     } catch (err) {
-        console.error('Error fetchin players', err);
+        console.error('Error fetching players', err);
         res.status(500).json({ error: `Server error` });
+    }
+});
+
+router.get('/name/:name', async (req, res) => {
+    try {
+        const name = req.params.name;
+        const player = await Player.findOne({ name: { $regex: new RegExp(`^${name}$`, 'i') } });
+
+        if (!player) {
+            return res.status(404).json({ error: 'Player not founf' });
+        }
+
+        res.json(player);
+    } catch (err) {
+        console.error('Error fetching player', err);
+        res.status(500).json({ error: 'Server error' });
     }
 });
 
