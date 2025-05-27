de8417d39ae4f9b41af4b80a5cbc31550846c260
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-27 14:42:06 -0500

    Updated PlayerPage to pull data from the api and display it.

--- client/src/components/PlayerPage.js
+++ client/src/components/PlayerPage.js
@@ -2,15 +2,31 @@
 // By: Sam Schmitz
 // Displays information about a player
 
-import { useParams} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function PlayerPage() {
     const name = useParams();
+    const [player, setPlayer] = useState();
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/players/name/{name}`)
+            .then(res => setPlayer(res.data))
+            .catch(err => console.error('Error fetching players:', err));
+    })
 
 
     return (
         <div className="Player Page" >
             <h1> {name}</h1>
+            <p>Team: {player.team }</p>
+            <p>Number: {player.jersey_number}</p>
+            <p>Position: {player.position}</p>
+            <p>Height: {player.height}</p>
+            <p>Weight: {player.weight}</p>
+            <p>Nationality: {player.nationality}</p>
+            <p>Date of Birth: {player.dob}</p>
         </div>
     );
 }
