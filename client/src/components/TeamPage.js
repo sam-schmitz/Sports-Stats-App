7458712aeb3ac2e4f45dd3a905bdb56eda03b668
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-28 11:42:53 -0500

    Added the ability for TeamPage to pull and display data from the api.

--- client/src/components/TeamPage.js
+++ client/src/components/TeamPage.js
@@ -7,8 +7,31 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 function TeamPage() {
+    const name = useParams().name;
+    const [team, setTeam] = useState(null);
+
+    useEffect(() => {
+        const uriName = encodeURIComponent(name);
+        const uri = `http://localhost:5000/teams/name/` + uriName;
+        axios.get(uri)
+            .then(res => setTeam(res.data))
+            .catch(err => console.error('Error fetching team:', err));        
+    }, [name])
+
     return (
         <div className="TeamPage" >
+            <h1>{name}</h1>
+            {team ? (
+                <>
+                    <p>Conference: {team.conference}</p>                 
+                    <p>Location: {team.location}</p>
+                    <p>Stadium: {team.stadium</p>
+                </>
+            ) : (
+                <>
+                    <p>Loading...</p>
+                </>
+            )}
         </div>
     );
 }
