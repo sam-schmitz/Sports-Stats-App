ffce77fe521a60925cdeaa322136b5b8e4a64560
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-28 11:29:39 -0500

    Created TeamPage to display information about an individual team.

---
+++ client/src/components/TeamPage.js
@@ -0,0 +1,16 @@
+// TeamPage.js
+// By: Sam Schmitz
+// Displays information about a team
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+function TeamPage() {
+    return (
+        <div className="TeamPage" >
+        </div>
+    );
+}
+
+export default TeamPage;
