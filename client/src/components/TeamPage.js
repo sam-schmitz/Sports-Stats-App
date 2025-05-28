df23fe64a5bd202313713e75a88e2adddcfd8d56
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-28 14:31:22 -0500

    Team Page can now pull a list of players from the api and display them.

--- client/src/components/TeamPage.js
+++ client/src/components/TeamPage.js
@@ -9,14 +9,25 @@ import axios from 'axios';
 function TeamPage() {
     const name = useParams().name;
     const [team, setTeam] = useState(null);
+    const [players, setPlayers] = useState([]);
 
     useEffect(() => {
         const uriName = encodeURIComponent(name);
         const uri = `http://localhost:5000/teams/name/` + uriName;
         axios.get(uri)
             .then(res => setTeam(res.data))
-            .catch(err => console.error('Error fetching team:', err));        
+            .catch(err => console.error('Error fetching team:', err));      
+        
     }, [name])
+
+    useEffect(() => {
+        if (team && team._id) {
+            console.log(`http://localhost:5000/teams/id/${team._id}/players`)
+            axios.get(`http://localhost:5000/teams/id/${team._id}/players`)
+                .then(res => setPlayers(res.data))
+                .catch(err => console.error('Error fetching team players:', err));
+        }
+    }, [team]);
 
     return (
         <div className="TeamPage" >
@@ -39,6 +50,20 @@ function TeamPage() {
             ) : (
                 <>
                     <p>Loading Image...</p>
+                </>
+            )}
+            {players.length > 0 ? (
+                <>
+                    <h3>Players</h3>
+                    <ul>
+                        {players.map(player => (
+                            <li key={player._id}>{player.name} - {player.position}</li>
+                        )) }
+                    </ul>
+                </>
+            ) : (
+                <>
+                    <p>Loading Players...</p>
                 </>
             )}
         </div>
