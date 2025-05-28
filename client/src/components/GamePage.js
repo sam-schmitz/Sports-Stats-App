3dadca8430217e74d0b7a33519102788d454d40e
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-05-28 12:54:07 -0500

    Created GamePage.js to display game information.

---
+++ client/src/components/GamePage.js
@@ -0,0 +1,13 @@
+// GamePage.js
+// By: Sam Schmitz
+// Displays information about a game
+
+function GamePage() {
+    return (
+        <div className="Game Page">
+            <h1>Game Page</h1>
+        </div>
+    );
+}
+
+export default GamePage;
