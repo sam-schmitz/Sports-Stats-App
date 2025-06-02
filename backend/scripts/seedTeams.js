18922ef00ba4263981b086e60ee3be318fe41ed3
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-06-02 13:37:42 -0500

    updated seedTeams to use a deeper endpoint in the api so that it could pull more specfic info about each team.

--- backend/scripts/seedTeams.js
+++ backend/scripts/seedTeams.js
@@ -15,28 +15,42 @@ const fetchNBATeams = async () => {
     return response.data.teams || [];
     */
     try {
-        const url = 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams';
-        const res = await axios.get(url);
-        const teams = res.data.sports[0].leagues[0].teams.map(t => t.team);
-        console.log(teams);
+        const baseTeamUrl = 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams';
+        const { data } = await axios.get(baseTeamUrl);
+        const teamList = data.sports[0].leagues[0].teams.map(t => t.team);
+        //console.log(teamList);
 
-        const formattedTeams = teams.map(team => ({
-            _id: team.id,
-            sport: 'basketball',
-            name: team.displayName,
-            abbreviation: team.abbreviation,
-            city: team.location,
-            conference: team.conference?.name || null,
-            division: team.division?.name || null,
-            stadium: team.venue?.fullName || null,
-            location: team.location,
-            logo: team.logos?.[0]?.href || null
-        }));
+        const detailedTeams = [];
 
-        console.log(formattedTeams);
-        return formattedTeams;
+        for (const team of teamList) {
+            const id = team.id;
+            const detailUrl = `${baseTeamUrl}/${id}`;
+            console.log(detailUrl);
+
+            const teamRes = await axios.get(detailUrl);
+            const teamData = teamRes.data.team
+
+            detailedTeams.push({
+                _id: teamData.id,
+                sport: 'basketball',
+                name: teamData.displayName,
+                abbreviation: teamData.abbreviation,
+                city: teamData.location,
+                conference: teamData.conference?.name || null,
+                division: teamData.division?.name || null,
+                stadium: teamData.franchise.venue?.fullName || null,
+                location: teamData.franchise.venue
+                    ? `${teamData.franchise.venue.address.city}, ${teamData.franchise.venue.address.state}`
+                    : null,
+                logo: teamData.logos?.[0]?.href || null,
+                espn_id: teamData.id
+            });
+        }        
+
+        console.log(detailedTeams);
+        return detailedTeams;
     } catch (err) {
-        console.error('Error fetching NBA teams:', err.message);
+        console.error('Error fetching NBA team details:', err.message);
     }
     
 };
