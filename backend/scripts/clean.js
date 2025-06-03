a44816d41a5601e1cec4401a310a51707fdad5f2
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-06-03 17:50:33 -0500

    Forgot imports...

--- backend/scripts/clean.js
+++ backend/scripts/clean.js
@@ -2,11 +2,16 @@
 // By: Sam Schmitz
 // Used to clean db of bad data
 
+const mongoose = require('mongoose');
+const Player = require('../models/Player');
+require('dotenv').config();
+
 const clean = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
 
-        await Player.deleteMany({ team: 'Arsenal' });
+        await Player.deleteMany({ team_name: 'Arsenal' });
+        console.log('Arsenal players removed');
 
         mongoose.disconnect();
     } catch (err) {
