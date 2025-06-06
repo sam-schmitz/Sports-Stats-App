8052c4d9208e20a3c769a3bed2663a83bc673fae
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-06-06 13:27:45 -0500

    Placed the team stats in their own object, TeamStats.

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -8,6 +8,34 @@ import axios from 'axios';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL;
 
+function TeamStats({ team }) {
+    return (
+        <>
+            <p>{team.name}</p>
+            <p><strong>Points: </strong>{team.points}</p>
+            <p><strong>Rebounds: </strong>{team.rebounds}</p>
+            <p><strong>Assists: </strong>{team.avgRebounds}</p>
+
+            <p><strong>Field Goal %: </strong>{team.fieldGoalPct}%</p>
+            <p><strong>Field Goals Made: </strong>{team.fieldGoalsMade}</p>
+            <p><strong>Field Goals Attempted: </strong>{team.fieldGoalsAttempted}</p>
+
+            <p><strong>Free Throw %: </strong>{team.freeThrowPct}%</p>
+            <p><strong>Free Throws Made: </strong>{team.freeThrowsMade}</p>
+            <p><strong>Free Throws Attempted: </strong>{team.freeThrowsAttempted}</p>
+
+            <p><strong>Three Point %: </strong>{team.threePointPct}%</p>
+            <p><strong>Three Point Field Goals Made: </strong>{team.threePointFieldGoalsMade}</p>
+            <p><strong>Three Point Field Goals Attempted: </strong>{team.threePointFieldGoalsAttempted}</p>
+
+            <p><strong>Average Points: </strong>{team.avgPoints}</p>
+            <p><strong>Average Rebounds: </strong>{team.avgRebounds}</p>
+            <p><strong>Average Assists: </strong>{team.avgAssists}</p>
+
+        </>
+    );
+}
+
 function GamePage() {
     const id = useParams().id;
     const [game, setGame] = useState(null);
@@ -47,29 +75,7 @@ function GamePage() {
                 <div className='row gx-0'>
                     <div className='col-sm-6 col-md-6' style={{ marginLeft: '5px', textAlign: 'left' }} >
                         {game ? (
-                            <>
-                                <p>{game.teams[0].name}</p>
-                                <p><strong>Points: </strong>{game.teams[0].points}</p>
-                                <p><strong>Rebounds: </strong>{game.teams[0].rebounds}</p>
-                                <p><strong>Assists: </strong>{game.teams[0].avgRebounds}</p>
-
-                                <p><strong>Field Goal %: </strong>{game.teams[0].fieldGoalPct}%</p>
-                                <p><strong>Field Goals Made: </strong>{game.teams[0].fieldGoalsMade}</p>
-                                <p><strong>Field Goals Attempted: </strong>{game.teams[0].fieldGoalsAttempted}</p>
-                                
-                                <p><strong>Free Throw %: </strong>{game.teams[0].freeThrowPct}%</p>                                
-                                <p><strong>Free Throws Made: </strong>{game.teams[0].freeThrowsMade}</p>
-                                <p><strong>Free Throws Attempted: </strong>{game.teams[0].freeThrowsAttempted}</p>
-
-                                <p><strong>Three Point %: </strong>{game.teams[0].threePointPct}%</p>
-                                <p><strong>Three Point Field Goals Made: </strong>{game.teams[0].threePointFieldGoalsMade}</p>
-                                <p><strong>Three Point Field Goals Attempted: </strong>{game.teams[0].threePointFieldGoalsAttempted}</p>
-                                
-                                <p><strong>Average Points: </strong>{game.teams[0].avgPoints}</p>
-                                <p><strong>Average Rebounds: </strong>{game.teams[0].avgRebounds}</p>
-                                <p><strong>Average Assists: </strong>{game.teams[0].avgAssists}</p>                                
-
-                            </>
+                            <TeamStats team={game.teams[0] } />
                         ) : (
                             <>
                                 <p>Loading...</p>
@@ -78,29 +84,7 @@ function GamePage() {
                     </div>
                     <div className='col-sm-5 col-md-5' style={{ marginLeft: '5px', textAlign: 'left' }} >
                         {game ? (
-                            <>
-                                <p>{game.teams[1].name}</p>
-                                
-                                <p><strong>Points: </strong>{game.teams[1].points}</p>
-                                <p><strong>Rebounds: </strong>{game.teams[1].rebounds}</p>
-                                <p><strong>Assists: </strong>{game.teams[1].avgRebounds}</p>
-
-                                <p><strong>Field Goal %: </strong>{game.teams[1].fieldGoalPct}%</p>
-                                <p><strong>Field Goals Made: </strong>{game.teams[1].fieldGoalsMade}</p>
-                                <p><strong>Field Goals Attempted: </strong>{game.teams[1].fieldGoalsAttempted}</p>
-
-                                <p><strong>Free Throw %: </strong>{game.teams[1].freeThrowPct}%</p>
-                                <p><strong>Free Throws Made: </strong>{game.teams[1].freeThrowsMade}</p>
-                                <p><strong>Free Throws Attempted: </strong>{game.teams[1].freeThrowsAttempted}</p>
-
-                                <p><strong>Three Point %: </strong>{game.teams[1].threePointPct}%</p>
-                                <p><strong>Three Point Field Goals Made: </strong>{game.teams[1].threePointFieldGoalsMade}</p>
-                                <p><strong>Three Point Field Goals Attempted: </strong>{game.teams[1].threePointFieldGoalsAttempted}</p>
-
-                                <p><strong>Average Points: </strong>{game.teams[1].avgPoints}</p>
-                                <p><strong>Average Rebounds: </strong>{game.teams[1].avgRebounds}</p>
-                                <p><strong>Average Assists: </strong>{game.teams[1].avgAssists}</p>                                
-                            </>
+                            <TeamStats team={game.teams[1] } />
                         ) : (
                             <>
                                 <p>Loading...</p>
