811185ae61abf041d3be5fb7b998f53bf1b764bc
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-07-31 16:42:50 -0500

    Updated the Home page. Added links to games, teams, and players. Added a note about the server hosting.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,37 @@
 // components/Home.js
 // By: Sam Schmitz
 
+import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className="Homepage">
-            <h1>Welcome to the Sports Stats App</h1>
+            <h1 className="mx-auto">Welcome to the Sports Stats App: </h1>
+            <hr />
+
+            <div className="row mx-auto text-center">
+                <div className="col-4 ">
+                    <h3><Link to="/Sports-Stats-App/teams">Teams</Link>  </h3>
+                </div>
+                <div className="col-4">
+                    <h3><Link to="/Sports-Stats-App/players">Players</Link>  </h3>
+                </div>
+                <div className="col-4">
+                    <h3><Link to="/Sports-Stats-App/games">Games</Link>  </h3>
+                </div>                                                
+            </div>
+
+            <div className="d-flex justify-content-center align-items-center" style={{height: '50vh'} }>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Basketball.png"
+                    alt="Basketball"
+                    style={{ width: '250px', height: '250px' }}
+                />
+            </div>
+
+            <div className="fixed-bottom text-right">
+                <p><strong>Note: </strong>The server is hosted for free so it may take up to 1 minute to load data. </p>
+                <p><strong>Disclaimer: </strong>For Educational Use Only</p>
+            </div>
         </div>        
     );
 }
